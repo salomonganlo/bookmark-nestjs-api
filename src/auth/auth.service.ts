8f67f12c660d5758c325996ceb03f0b166b7c1fd
salomonganlo <salomonganlo@gmail.com>
2022-10-09 19:17:19 +0100

    feat: signup created  successfully

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
+  constructor(private prisma: PrismaService) {}
+  async signin() {
     return {
       msg: 'I have signed in',
     };
   }
-  signup() {
-    return {
-      msg: 'I have signup ',
-    };
+  async signup(dto: AuthDto) {
+    const hashPassword = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashPassword,
+          firstname: dto.firstname,
+          lastname: dto.lastname,
+        },
+      });
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists!');
+        }
+        throw error;
+      }
+    }
   }
 }
